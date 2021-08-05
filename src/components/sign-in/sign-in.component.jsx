0b77851c5948975d8b97aa46e91221415c5f38ad
Matt <80971715+mattbeez@users.noreply.github.com>
2021-08-05 12:03:38 -0600

    implemented firebase utils, ability to store authenticated users and add users.  fixed previous errors as well.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,54 +1,67 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
-
 import CustomButton from '../custom-button/custom-button.component';
 
 import { signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.style.scss';
+import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: '',
-            passoword: ''
-        }
-    }
-    handleSubmit = event => {
-        event.preventDefault();
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
 
-        this.setState({ email: '', password: '' });
-    }
+  handleSubmit = event => {
+    event.preventDefault();
 
-    handleChange = event => {
-        const { value, name } = event.target;
+    this.setState({ email: '', password: '' });
+  };
 
-        this.setState({ [name]: value });
-    }
+  handleChange = event => {
+    const { value, name } = event.target;
 
-    render() {
-        return (
-            <div className='sign-in'>
-            <h2>I already have an account</h2>
-            <span>Sign in with your email and password</span>
+    this.setState({ [name]: value });
+  };
 
-            <form onSubmit={this.handleSubmit}>
-            <FormInput name='email' type='email' value={this.state.email} label='email' handleChange={this.handleChange} required />
-            
-            <FormInput name='password' type='password' value={this.state.email} label='password' handleChange={this.handleChange} required />
-         
-            <div className='buttons'>
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <div className='buttons'>
             <CustomButton type='submit'> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
-            </div>
-            
-            </form>
-            </div>
-        );
-    }
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
--- src/firebase/firebase.utils.jsx
+++ src/firebase/firebase.utils.jsx
@@ -3,7 +3,6 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-
     apiKey: "AIzaSyBDwukuQ9sVPB0v4Cyj5cXRalEqA30k3Ls",
     authDomain: "crwn-db-33f3b.firebaseapp.com",
     projectId: "crwn-db-33f3b",
@@ -13,13 +12,38 @@ const config = {
     measurementId: "G-0P7EWGL89E"
 };
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
