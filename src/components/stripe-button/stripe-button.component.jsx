1304088ac3be588be8da3a5c56e0ec5af4590b24
Matt <80971715+mattbeez@users.noreply.github.com>
2021-08-20 12:26:12 -0600

    added stripe integration and updated title and icon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JQbn8DnYi6ax0fL0P3uOJXT6zuivMQyDMPnockPpMtSxI2hePrYTZ7cGdEJ7hO0DBcL4ErwAs5bTJhP1avpR22Z00DMyEgbA1";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
